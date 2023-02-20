3058b812d1d572e63671b4ff353225cba330f8d5
KaTang-lu <katang346@gmail.com>
2023-02-21 02:12:38 +0700

    Update signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -5,9 +5,9 @@ include($_SERVER['DOCUMENT_ROOT'] . '/pizza-database-project/components/connect.
 /* session_start();
 
 if(isset($_SESSION['user_id'])){
-   $user_id = $_SESSION['user_id'];
+    $user_id = $_SESSION['user_id'];
 }else{
-   $user_id = '';
+    $user_id = '';
 };
 */
 
@@ -33,21 +33,20 @@ if(isset($_POST['submit'])){
     $select_user->execute([$email, $phone]);
     $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
-   if($select_user->rowCount() > 0){
-      $message[] = 'email or number already exists!';
-   }else{
-      if($password != $confirm_password){
-         $message[] = 'confirm password not matched!';
-      }else{
-         $insert_user = $conn->prepare("INSERT INTO `customer` (cus_fname, cus_lname, cus_phone, cus_birthdate, cus_gender, cus_email, cus_password) VALUES (?, ?, ?, ?, ?, ?, ?)");
-         $insert_user->execute([$fname, $lname, $phone, $birthdate, $gender, $email, $password]);
-         $select_user = $conn->prepare("SELECT * FROM `customer` WHERE email = ? AND password = ?");
-         $select_user->execute([$email, $password]);
-         $row = $select_user->fetch(PDO::FETCH_ASSOC);
-         header('Location: home.php');
-      }
-   }
-
+    if($select_user->rowCount() > 0){
+        $message[] = 'email or number already exists!';
+    }else{
+        if($password != $confirm_password){
+            $message[] = 'confirm password not matched!';
+        }else{
+            $insert_user = $conn->prepare("INSERT INTO `customer` (cus_fname, cus_lname, cus_phone, cus_birthdate, cus_gender, cus_email, cus_password) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $insert_user->execute([$fname, $lname, $phone, $birthdate, $gender, $email, $password]);
+            $select_user = $conn->prepare("SELECT * FROM `customer` WHERE email = ? AND password = ?");
+            $select_user->execute([$email, $password]);
+            $row = $select_user->fetch(PDO::FETCH_ASSOC);
+            header('Location: home.php');
+        }
+    }
 }
 
 ?>
@@ -55,24 +54,25 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Sign Up</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head>
 
-   <!-- bootstrap  -->
-   
+    <!-- bootstrap  -->
+
 </head>
 <body>
-   
+
 <!-- header section starts  
-php include 'components/user_header.php'; ?>
+php include 'components/user_header.php'; ?> -->
 <!-- header section ends -->
 
 <section class="form-container">
@@ -80,45 +80,60 @@ php include 'components/user_header.php'; ?>
         <form method="post" action="">
             <fieldset name="register">
             <legend>Register</legend>
-            <div class="form-group" style="text-align:center;" >
-                <input type="text" name="fname" required placeholder="First name" class="box" maxlength="50">
+            <hr>
+
+            <!-- F/L name -->
+            <label for="firstname" class="form-label">Name</label>
+            <div class="row g-3">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+                </div>
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="text" name="lname" required placeholder="Last name" class="box" maxlength="50">
+            
+            <div class="mb-3"> <!-- Phone number -->
+                <label for="phonenumber" class="form-label">Phone number</label>
+                <input type="text" class="form-control" name="tel" placeholder="+66">
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="tel" name="phone" required placeholder="Phone number" class="box" maxlength="10">
+
+            <div class="mb-3"> <!-- Date of birth -->
+                <label for="startDate">Date of birthday</label>
+                <input id="startDate" class="form-control" type="date" />
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="date" name="birthdate" required placeholder="Date of birth" class="box">
+
+            <!-- Gender -->
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                <label class="form-check-label" for="inlineRadio1">Male</label>
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="radio" name="gender" value="male" required>
-                <span>Male</span>
-                <input type="radio" name="gender" value="female" required>
-                <span>Female</span>
-                <input type="radio" name="gender" value="none" required>
-                <span>None</span>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                <label class="form-check-label" for="inlineRadio1">Female</label>
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="email" name="email" required placeholder="Email address" class="box" maxlength="50">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                <label class="form-check-label" for="inlineRadio1">None</label>
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="password" name="password" required placeholder="Password" class="box" maxlength="20">
+
+            <div class="mb-3"> <!-- Email -->
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" aria-describedby="email">
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="password" name="confirm_password" required placeholder="Confirm password" class="box" maxlength="20">
+            <div class="mb-3"> <!-- Password -->
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password">
             </div>
-            <div class="form-group" style="text-align:center;">
-                <input type="checkbox" name="agree" required>
-                <span>I have read and accepted terms & conditions and privacy statement of The Pizza Company</span>
-                <br>
-                <input type="checkbox" name="receive_info">
-                <span>I agree to receive the information including other marketing activities from The Pizza Company<br>and affiliated companies. We will keep your data confidential. Learn more about privacy statement from<br>company’s website.</span>
+            <div class="mb-3"> <!-- Confirm Password -->
+                <label for="confirm password" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" name="c_password">
             </div>
+            
             <div class="form-group" style="text-align:center;" style="display: flex; justify-content: center;">
-                <input type="submit" name="submit" value="Register" class="button">
+                <button type="submit" name="signup" class="btn btn-primary">Sign Up</button>
             </div>
+            
             </fieldset>
         </form>
     </div>
