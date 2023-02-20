ea404b181b3ac7dee79dc890fb259f1f5940517e
Ratchanon <ratchanonmcnon2015@gmail.com>
2023-02-21 04:43:13 +0700

    add register button

--- pages/login.php
+++ pages/login.php
@@ -65,19 +65,22 @@ if(isset($_POST['submit'])) {
 <html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Login Page</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Page</title>
 
-    <!-- custom css file link  -->
-    <link rel="stylesheet" href="css/style.css">
+
+  <!-- custom css file link  -->
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head>
 </head>
 <body>
     <form method="post" action="">
       <input type="email" name="email" required placeholder="Enter your email"><br><br>
       <input type="password" name="password" required placeholder="Enter your password"><br><br>
       <input type="submit" name="submit" value="Login">
+      <a href="signup.php" class="btn btn-primary">Register</a>
     </form>
   </body>
 </html>
