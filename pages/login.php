4cf0154540daa877767f4aea6f17535ccde2019d
Gapraomoosap <116646446+Gapraomoosap@users.noreply.github.com>
2023-02-26 22:35:03 +0700

    Updated frontend loginpage

--- pages/login.php
+++ pages/login.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['email'])) {
 
 require_once "../components/connect.php";
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
   $email = $_POST['email'];
   $password = $_POST['password'];
 
@@ -27,11 +27,11 @@ if(isset($_POST['submit'])) {
   $stne = $pdo->prepare("SELECT * FROM `customer` WHERE cus_email = ?");
   $stne->execute([$email]);
   $row = $stne->fetch(PDO::FETCH_ASSOC);
-  if($row){
+  if ($row) {
     $_SESSION['fname'] = $row['cus_fname'];
   }
 
-  if($row_count == 1) {
+  if ($row_count == 1) {
     $_SESSION['email'] = $email;
     header("Location: home.php");
   } else {
@@ -43,20 +43,19 @@ if(isset($_POST['submit'])) {
 
     $row_count = $stmt->rowCount();
 
-    if($row_count == 1) {
+    if ($row_count == 1) {
       $stne = $pdo->prepare("SELECT * FROM `employee` WHERE emp_email = ?");
       $stne->execute([$email]);
       $row = $stne->fetch(PDO::FETCH_ASSOC);
-      if($row){
+      if ($row) {
         $_SESSION['fname'] = $row['emp_name'];
       }
 
       $_SESSION['email'] = $email;
-      header("Location: /pizza-database-project/admin/admin.php"); 
+      header("Location: /pizza-database-project/admin/admin.php");
     } else {
       echo "Invalid login credentials";
     }
-
   }
 }
 ?>
@@ -64,7 +63,9 @@ if(isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html>
 <html lang="en">
+
 <head>
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -72,15 +73,33 @@ if(isset($_POST['submit'])) {
 
 
   <!-- custom css file link  -->
-  <link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head>
+  <link rel="stylesheet" type="text/css" href="../css/login_style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
 <body>
-    <form method="post" action="">
-      <input type="email" name="email" required placeholder="Enter your email"><br><br>
-      <input type="password" name="password" required placeholder="Enter your password"><br><br>
-      <input type="submit" name="submit" value="Login">
-      <a href="signup.php" class="btn btn-primary">Register</a>
-    </form>
-  </body>
+    <div class="container">
+        <div class="row vh-100 align-items-center justify-content-center">
+            <div class="col-sm-8 col-md-6 col-lg-4 bg-white rounded border border-success  p-4 shadow">
+                <div class="row justify-content-center mb-4">
+                    <h1 class="heading text-center">Login</h1>
+                </div>
+                <form>
+                    <div class="mb-4">
+                        <label for="email" class="form-label">Email </label>
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-4">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" aria-describedby="emailHelp">
+                    </div>
+                    <button type="submit" class="btn btn-success w-100">Login</button>
+                </form>
+                <p class = "mb-0 text-center">Not registered yet? <a href="signup.php" class="text-decoration-none">Signup here</a></p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+
 </html>
