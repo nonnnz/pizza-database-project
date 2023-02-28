ac697d2d15065f4589f8b7fca657740bddb346c3
Ratchanon <ratchanonmcnon2015@gmail.com>
2023-02-28 12:37:53 +0700

    Add user header, admin header

---
+++ components/admin_header.php
@@ -0,0 +1,14 @@
+<div class="header" >
+   <div class="inner_header">
+      <div class="logo_container">
+            <h1>112Pizza</h1>
+      </div>
+      <ul class = "navigation">
+            <li><a href="../admin/admin.php">Dashboard</a></li>
+            <li><a href="../admin/user_management.php">User management</a></li>
+            <li><a href="#">Order management</a></li>
+            <li><a href="../admin/menu_management.php">Menu management</a></li>
+            <li><a href="../pages/logout.php">Logout</a></li>
+      </ul>
+   </div>
+</div>
--- components/user_header.php
+++ components/user_header.php
@@ -4,15 +4,15 @@
             <h1>112Pizza</h1>
       </div>
       <ul class = "navigation">
-            <li><a href="#">Menu</a></li>
+            <li><a href="home.php">Menu</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Contact</a></li>
-            <?php if(isset($_SESSION['email'])): ?>
+            <?php if(isset($_SESSION['user_id'])): ?>
                <li><a href="#">Orders</a></li>
                <li><a href="#">Profile</a></li>
-               <li><a href="\logout.php">Logout</a></li>
+               <li><a href="logout.php">Logout</a></li>
             <?php else: ?>
-               <li><a href="\login.php">Login</a></li>
+               <li><a href="login.php">Login</a></li>
             <?php endif; ?>
       </ul>
    </div>
