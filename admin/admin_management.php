01047521a7ffb4d661800f7334f26b457f4f256e
Ratchanon <ratchanonmcnon2015@gmail.com>
2023-03-07 00:53:40 +0700

    Change css location

--- admin/admin_management.php
+++ admin/admin_management.php
@@ -43,7 +43,7 @@ $users = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Page</title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head>
 </head>
 </head>
--- admin/menu_management.php
+++ admin/menu_management.php
@@ -83,7 +83,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Menu Management</title>
   <!-- CSS -->
-  <!-- <link rel="stylesheet" type="text/css" href="../css/style.css"> -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
