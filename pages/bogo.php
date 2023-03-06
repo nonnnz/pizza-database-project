b30c8be521410bc6662e468bcea21d035fea071f
Gapraomoosap <peelasin2544@gmail.com>
2023-03-07 00:34:11 +0700

    Update bogo page

--- pages/bogo.php
+++ pages/bogo.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bogo</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/script.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -15,27 +16,39 @@
 
     <div class="home-category" style = "margin-top: 80px ">
 
-        <h5 style = "margin-left : 6em;">Menu</h5>
-        <div class="category_container" style = "font-size : 10px;">
-            
-            <div class="bcate cate1" >
-                <h6 id="bottom">Pizza</h6>
-            </div>
-            <div class="bcate cate2">
-                <h6 id="bottom">Pizza of the<br>month</h6>
-            </div>
-            <div class="bcate cate3">
-                <h6 id="bottom">Appetizer</h6>
-            </div>
-            <div class="bcate cate4">
-                <h6 id="bottom">Chicken</h6>
-            </div>
-            <div class="bcate cate5">
-                <h6 id="bottom">Pasta</h6>
-            </div>
-            <div class="bcate cate6">
-                <h6 id="bottom">Salad & Steak</h6>
-            </div>
+        <h4 style = "margin-left : 6em;"> <img src="https://cdn.1112.com/1112/public/images/web/menu.svg" alt="Cheese"> </img>
+            Menu</h4>
+        <div class="category_container" >
+            <a href="#">
+                <div class="box bbox1">
+                    <h6>Pizza</h6>
+                </div>
+            </a>
+            <a href="#">
+                <div class="box2 bbox2">
+                    <h6 id="bottom">Appetizer</h6>
+                </div>
+            </a>
+            <a href="#">
+                <div class="box2 bbox3">
+                    <h6 id="bottom">Chicken</h6>
+                </div>
+            </a>
+            <a href="#">
+                <div class="box2 bbox4">
+                    <h6 id="bottom">Pasta</h6>
+                </div>
+            </a>
+            <a href="#">
+                <div class="box2 bbox5">
+                    <h6 id="bottom">Salad & Steak</h6>
+                </div>
+            </a>
+            <a href="#">
+                <div class="box2 bbox6">
+                    <h6 id="bottom">Drink & <br>Desserts</h6>
+                </div>
+            </a>
             
         </div>
     </div>
@@ -46,88 +59,65 @@
         <div class="product_container" >
             <div class="product">
                 <div class="product_item">
-                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Oct2021/102216_MP.png" alt="Cheese" style = "width : 100% ; height : 100% ;">
+                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Oct2021/102216_MP.png" alt="Cheese" style = "width : 100% ;  height : 217px ;">
+                   
+                </div>
+                <div class="item-footer">
                     <h5>Double Cheese</h5>
+                    <button type="button" class="btn btn-success d-flex justify-content-between" style ="width: 80%;height: 40px;  margin-left: 0.5rem; padding: 0.5rem;">
+                        <span>439 ฿</span>
+                        <span>
+                            <i class="fa fa-plus right txt" style="margin-right: 5px;"></i>Select
+                        </span>
+                    </button>
                 </div>
-                
-                <div class="p_footer">
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="price_left">
-                            <span class="left price" style = "color : white;">299 ฿</span>
-                        </div>
-                    </a>
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="select_right">
-                            <span class="right txt" style = "color : white;"><i class="fa fa-plus"></i> Select</span>
-                        </div>
-                    </a>
-                    
-                </div>
-                
             </div>
+            
             <div class="product">
                 <div class="product_item">
-                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Oct2021/102217_MP.png" alt="Cheese" style = "width : 100% ; height : 100% ;">
+                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Oct2021/102217_MP.png" alt="Cheese" style = "width : 100% ;  height : 217px ;">
+                    
+                </div>
+                <div class="item-footer">
                     <h5>Double Pepperoni</h5>
+                    <button type="button" class="btn btn-success d-flex justify-content-between" style ="width: 80%;height: 40px;  margin-left: 0.5rem; padding: 0.5rem;">
+                        <span>439 ฿</span>
+                        <span>
+                            <i class="fa fa-plus right txt" style="margin-right: 5px;"></i>Select
+                        </span>
+                    </button>
                 </div>
-                
-                <div class="p_footer">
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="price_left">
-                            <span class="left price" style = "color : white;">299 ฿</span>
-                        </div>
-                    </a>
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="select_right">
-                            <span class="right txt" style = "color : white;"><i class="fa fa-plus"></i> Select</span>
-                        </div>
-                    </a>
-                    
-                </div>
-                
             </div>
             <div class="product">
                 <div class="product_item">
-                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Feb2023/216296_EGM_1.png" alt="Cheese" style = "width : 258.67px ; height : 185.72px ;">
-                    <h5>Seafood Bacon</h5>
-                </div>
-                
-                <div class="p_footer">
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="price_left">
-                            <span class="left price" style = "color : white;">539 ฿</span>
-                        </div>
-                    </a>
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="select_right">
-                            <span class="right txt" style = "color : white;"><i class="fa fa-plus"></i> Select</span>
-                        </div>
-                    </a>
+                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Oct2021/102208_MP.png" alt="Cheese" style = "width : 100% ; height : 217px ;">
                     
                 </div>
-                
+                <div class="item-footer">
+                    <h5>Seafood Cocktail</h5>
+                    <button type="button" class="btn btn-success d-flex justify-content-between" style ="width: 80%;height: 40px;  margin-left: 0.5rem; padding: 0.5rem;">
+                        <span>439 ฿</span>
+                        <span>
+                            <i class="fa fa-plus right txt" style="margin-right: 5px;"></i>Select
+                        </span>
+                    </button>
+                </div>
             </div>
             <div class="product">
                 <div class="product_item">
-                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Nov2022/199446.png" alt="Cheese" style = "width : 100% ; height : 185.72px ;">
-                    <h5>Grilled Hawaiian</h5>
+                    <img src="https://cdn.1112.com/1112/public//images/products/pizza/Dec2021/102734.png" alt="Cheese" style = "width : 100% ; height : 217px  ;">
                 </div>
-                
-                <div class="p_footer">
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="price_left">
-                            <span class="left price" style = "color : white;">509 ฿</span>
-                        </div>
-                    </a>
-                    <a href="#" style = "text-decoration: none;">
-                        <div class="select_right">
-                            <span class="right txt" style = "color : white;"><i class="fa fa-plus"></i> Select</span>
-                        </div>
-                    </a>
-                    
+                <div class="item-footer">
+                    <h5>Spicy Super Seafood</h5>
+                    <button type="button" class="btn btn-success d-flex justify-content-between" style ="width: 80%;height: 40px;  margin-left: 0.5rem; padding: 0.5rem;">
+                        <span>439 ฿</span>
+                        <span>
+                            <i class="fa fa-plus right txt" style="margin-right: 5px;"></i>Select
+                        </span>
+                    </button>
                 </div>
-                
             </div>
+            
             
             
         </div>
