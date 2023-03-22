7bd7db756f2df7644b13913c44d4b73fd909a92c
Gapraomoosap <peelasin2544@gmail.com>
2023-03-22 14:50:40 +0700

    Update credit-card page (124 line display : none)

--- pages/credit-card.php
+++ pages/credit-card.php
@@ -121,6 +121,47 @@ if(isset($_SESSION['user_id'])){
         </div>
     </div>
 
+    <div class="credit_container " style = "display : none">
+        <div class="adress-head d-flex justify-content-between ">
+            <h2>CREDIT CARD</h2>
+            <button type="button " class="btn btn-success " style="height: 40px; margin-left: 0.5rem; padding: 0.5rem; ">
+                <i class="fa fa-plus right txt " style="margin-right: 5px; "></i>Add New Card
+
+            </button>
+        </div>
+
+        <div class="credit-box ">
+            <div class="address-detail ">
+                <div class="d-flex flex-column ">
+                    <div class="p d-flex ">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAAAgCAMAAABq4atUAAAAhFBMVEX3nhvrABv/////XwD+7/D958XsDyj5tlT82qn/+fDwP1P+8+L+awXyIxD7zozwFxT5r7f1f4zyX2/uL0X9bwb+7dT3pCn93+ L3mRn6hhD+WQH81Jr5vGHtHzb4r0X94Lf7z9T1Lw35QQj8egv0b376v8X2kJv2X0P6wnD5ihL4bE74nyWXYNIuAAABPklEQVQ4jZ2V61KDMBCFFwIkBEWgAloubZXWqu//fnInJSd2yvnFZPLNHnaTE7IXRYncWVYlm2heSouSE2WcF4GykaaPPLEWebHTrR1aYFYt1pATW7fyLnagIp24ewPlO0vT1y9pEgp09HTmmT296dR1hhzIMEiFI+QAby+s06sO0WGA1j3otO8h9q5Dmd9BOTB3GhiTQbITnZkK4VJBCxm6MJYCf1XYFIFCHzPEgD9uE3K3MMgf+SRN/R70CSCXjENixlG5BNzdg8ItULHN3qZGNMDffoFwyx8fbrnxGD1+YP0WQvf29E+h8N4l/IaF+owwjgr2OxVDsFS4F+jeiiXCIGUMozEsUSD96GGZqWHZKl710ItALKfrB8BJFKy69A+AWypIPSW5ArU6N1J6npTxeV4KxJXXVPNS+MrGP15ME2pz0EKIAAAAAElFTkSuQmCC
+                            " alt=" ">
+                    <h4 style="padding-left: 10px;display: flex; justify-content: center; text-align: center; margin-bottom: 0 !important; ">xxxx xxxx xxxx xxxx</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="row pt-4 ">
+                <p>Card Name : PEERASIN SRISRI</p>
+            </div>
+            <div class="row ">
+                <p>Expiration Date : 01/01/01</p>
+            </div>
+            <div class="row pt-2 ">
+                <div class="col ">
+                    <input class="form-check-input " type="checkbox " value=" " id="flexCheckDefault ">
+                    <label class="form-check-label " for="flexCheckDefault " style="font-weight: bold; ">
+                        This is your default credit card
+                    </label>
+                </div>
+                
+                <div class="col-1 ">
+                    <a class="btn btn-inline btn-small remove "><img class="img-profile "
+                            src="https://1112.com/images/remove_icons.svg " style="width : 17px; "></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     
 
 </body>
